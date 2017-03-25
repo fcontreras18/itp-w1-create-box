24b3ca4e10e3d41093678d6c0c447effe946af95
Francisco Echeverria <fcontreras18@gmail.com>
2017-03-25 15:22:01 +0000

    initial commit

--- create_box/main.py
+++ create_box/main.py
@@ -2,8 +2,18 @@
 
 
 def create_box(height, width, character):
-    pass
+    if height >= 1 and width >= 1:
+        return (width * character + '\n') * height
+    return 'Height and width must be greater or equal to 1'
+
+def create_outer_border(height, width, character):
+    if height >= 1 and width >= 1:
+        top_and_bottom_lines = width * character + '\n'
+        middle_lines = character + ((width - 2) * ' ') + character + '\n'
+        return top_and_bottom_lines + (middle_lines * (height - 2)) + top_and_bottom_lines
+    return 'Height and width must be greater or equal to 1'
 
 
 if __name__ == '__main__':
     create_box(3, 4, '*')
+
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,10 @@
 import unittest
 
+# For an extra challenge, make it so only the outer border of the box shows 
+# and it is not filled in. Make this a separate function and add unit tests for it.
+
 from create_box import create_box
+from create_box import create_outer_border
 
 first_box_expected = """
 ****
@@ -18,6 +22,25 @@ xxxxxxxxxxxxxxxxxxxxxxxx
 xxxxxxxxxxxxxxxxxxxxxxxx
 """.lstrip()
 
+fourth_box_expected = """
+$$$$$$
+$    $
+$    $
+$    $
+$    $
+$$$$$$
+""".lstrip()
+
+fifth_box_expected = """
+#####
+#   #
+#   #
+#   #
+#   #
+#   #
+#####
+""".lstrip()
+
 
 class TestCreateBox(unittest.TestCase):
     def test_box(self):
@@ -25,5 +48,15 @@ class TestCreateBox(unittest.TestCase):
 
     def test_small_box(self):
         self.assertEqual(create_box(1, 1, '@'), second_box_expected)
-
+        
+    def test_third_box(self):
+        self.assertEqual(create_box(3, 24, 'x'), third_box_expected)
+        
+        
+class TestCreateBorderBox(unittest.TestCase):
+    def test_border_box(self):
+        self.assertEqual(create_outer_border(6, 6, '$'), fourth_box_expected)
+    
+    def test_border_box2(self):
+        self.assertEqual(create_outer_border(7, 5, '#'), fifth_box_expected)
     # Add your own test using third_box_expected
